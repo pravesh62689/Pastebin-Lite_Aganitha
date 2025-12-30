56c6a3f99df5f632bf43d8ffdc1030012f91b7f6
Pravesh <pravesh@oritso.in>
2025-12-30 21:45:34 +0530

    frontend error resolved

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,10 +3,6 @@ import ReactDOM from "react-dom/client";
 import axios from "axios";
 import App from "./App.jsx";
 
-if (import.meta.env.VITE_API_BASE) {
-  axios.defaults.baseURL = import.meta.env.VITE_API_BASE;
-}
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
